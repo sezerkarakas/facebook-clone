f570ad1e741b001feb3f41cf787b1e0a0e164b70
sezerkarakas <sezerking123@gmail.com>
2024-03-20 15:33:09 +0300

    changes made

--- facebook-frontend/src/Pages/LoginPage.jsx
+++ facebook-frontend/src/Pages/LoginPage.jsx
@@ -1,17 +1,21 @@
+import { useState, useEffect } from "react";
 import "../index.css";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+
+  const [loginCredentials, setLoginCredentials] = useState({})
+
   return (
-    <div className="flex flex-row justify-center items-center h-dvh w-screen">
+    <div className="flex flex-row justify-center items-center h-dvh w-screen bg-slate-200">
       <div className="w-1/3 h-2/6 flex justify-start items-start flex-col">
-        <h1 className="text-6xl">facebook</h1>
-        <p className="text-3xl">
+        <h1 className="text-6xl text-blue-600 font-bold pb-5">facebook</h1>
+        <p className="text-3xl text-black">
           Facebook tanıdıklarınla iletişim kurmanı ve hayatında olup bitenleri
           paylaşmanı sağlar.
         </p>
       </div>
-      <div className="flex flex-col justify-center items-center w-1/5 h-2/5 bg-white  rounded-md shadow-gray-300 p-1 pb-5">
+      <div className="flex flex-col justify-center items-center w-1/5 h-2/5 bg-white  rounded-md shadow-gray-300 shadow-2xl border-gray-200 border-2 p-1 pb-5">
         <form className="flex flex-col justify-center items-center gap-5 w-full h-full">
           <input
             className="login-input"
---
+++ facebook-frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return <div className="h-1/6 flex flex-row justify-center items-center">
+    <h1>Footer</h1>
+  </div>;
+};
+
+export default Footer;
